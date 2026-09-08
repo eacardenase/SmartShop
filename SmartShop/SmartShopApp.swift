7483857a31f42122cf696c6997e93e22f8e44cb2
Edwin Cardenas <eacardenase@gmail.com>
2026-09-07 20:00:48 -0500

    Initial Commit

---
+++ SmartShop/SmartShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartShopApp.swift
+//  SmartShop
+//
+//  Created by Edwin Cardenas on 9/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SmartShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
